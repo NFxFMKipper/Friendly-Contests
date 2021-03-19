247df6a631b7120a1e06c881c8b5b2627f3fcf3a
NFxFMKipper <56832780+NFxFMKipper@users.noreply.github.com>
2021-03-19 10:46:51 +0530

    Update 337. House Robber III.cpp

--- FC1/337. House Robber III.cpp
+++ FC1/337. House Robber III.cpp
@@ -4,19 +4,18 @@ class Solution {
 public:
     int *max_rob(TreeNode* node)
     {
-        int arr[2];
+        int *arr = new int[2];
         if(node == nullptr)
         {
             arr[0] = arr[1] = 0;
             return arr;
         }
-        int *lef = max_rob(node->left);
-        int left[2];
-        left[0] = lef[0];
-        left[1] = lef[1];
+        int *left = max_rob(node->left);
         int *right = max_rob(node->right);
         arr[0] = max(left[0], left[1])+max(right[0], right[1]);
         arr[1] = node->val+left[0]+right[0];
+        delete[] left;
+        delete[] right;
         return arr;
     }
     
